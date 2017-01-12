00ba0e69e6bb7dbe71d4dc47dafb17664f0fac19
Marco Barisione <marco@barisione.org>
2017-01-12 17:51:07 +0000

    Add linting for the python code

---
+++ scripts/lint.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+if [ -e "../scripts/lint.sh" ]; then
+    cd ..
+fi
+
+if [ -e "../karton/karton.py" ]; then
+    cd ..
+fi
+
+if [ ! -e "./scripts/lint.sh" ]; then
+    echo "You should run $0 from the top-level Karton dir." >&2
+    exit 1
+fi
+
+pylint \
+    --rcfile=scripts/pylintrc \
+    karton/*.py
