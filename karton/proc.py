729546f278127c11d31f3d4bbd1de35c9588603f
Marco Barisione <marco@barisione.org>
2017-01-20 08:33:57 +0000

    Add a proc module to spawn processes

---
+++ karton/proc.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python
+#
+# Copyright (C) 2016-2017 Marco Barisione
+#
+# Released under the terms of the GNU LGPL license version 2.1 or later.
+
+import subprocess
+
+from log import verbose
+
+
+CalledProcessError = subprocess.CalledProcessError
+
+
+def call(cmd_args, *args, **kwargs):
+    '''
+    Like subprocess.call, but with extra logging in verbose mode.
+    '''
+    verbose('Calling (using call):\n%s' % cmd_args)
+    return subprocess.call(cmd_args, *args, **kwargs)
+
+
+def check_output(cmd_args, *args, **kwargs):
+    '''
+    Like subprocess.check_output, but with extra logging in verbose mode.
+    '''
+    verbose('Calling (using check_output):\n%s' % cmd_args)
+    return subprocess.check_output(cmd_args, *args, **kwargs)
