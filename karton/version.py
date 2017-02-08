cc5becb1010011e0c8608d48522fdb831b9cbc1e
Marco Barisione <marco@barisione.org>
2017-02-08 22:53:57 +0000

    Add version information

---
+++ karton/version.py
@@ -0,0 +1,12 @@
+# Copyright (C) 2017 Marco Barisione
+#
+# Released under the terms of the GNU LGPL license version 2.1 or later.
+
+from __future__ import print_function
+
+
+__version__ = '0.0.1'
+
+
+if __name__ == '__main__':
+    print(__version__)
