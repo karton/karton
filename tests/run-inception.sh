a4d68cbfcda6692a1c4d5704f3a0d48abbdcf73b
Marco Barisione <marco@barisione.org>
2017-02-13 08:51:38 +0000

    Check Karton can be installed and used inside inception

--- tests/run-inception.sh
+++ tests/run-inception.sh
@@ -19,10 +19,12 @@ package=$(basename "$package_path")
 base_package_name=$(echo "$package" | sed 's,.tar.gz$,,')
 
 rm test-results/inception-*.json 2> /dev/null
+rm test-results/sanity-*.log 2> /dev/null
 
-script_path=$(mktemp)
+main_script_path=$(mktemp)
+sanity_script_path=$(mktemp)
 
-cat > $script_path << EOF
+cat > $main_script_path << EOF
 #! /bin/bash
 
 set -e
@@ -30,14 +32,113 @@ set -e
 tar xzf "$package"
 cd "$base_package_name"
 
-python ./tests/run.py --save-json-results ../test-results.json
+do_sanity_check="false"
+
+touch ../test-results.json
+if python ./tests/run.py --save-json-results ../test-results.json; then
+    do_sanity_check="true"
+fi
+
+cd ..
+
+if [ "\$do_sanity_check" != "true" ]; then
+    echo "SKIPPED" > sanity-results.log
+    exit 0
+fi
+
+# Set the status as failed. This will be overwritten later if everything works.
+echo "FAILED" > sanity-results.log
+echo >> sanity-results.log
+echo 'OUTPUT OF THE SCRIPT:' >> sanity-results.log
+echo '~~~~~~~~~~~~~~~~~~~~~' >> sanity-results.log
+echo >> sanity-results.log
+
+./sanity.sh 2>&1 | tee --append --ignore-interrupts sanity-results.log
+
+if [ \${PIPESTATUS[0]} -eq 0 ]; then
+    # Overwrite the temporary failed stuff now that we know it did work.
+    echo "PASSED" > sanity-results.log
+fi
+EOF
+
+cat > $sanity_script_path << EOF
+set -e
+
+echo "INSTALLING"
+cd "$base_package_name"
+sudo python setup.py install
+cd ..
+echo
+
+echo "REMOVING PACKAGE AND STUFF"
+rm -r "$base_package_name"
+rm -r "$package"
+echo
+
+echo "INFO"
+echo "Karton is now available at \$(which karton)"
+echo
+
+echo "KARTON HELP"
+karton help
+echo
+
+echo "KARTON --VERSION"
+karton --version
+echo
+
+echo "KARTON IMAGE IMPORT"
+mkdir definition-dir/
+cat > definition-dir/definition.py << END
+import os
+
+def setup_image(props):
+    print 'Running setup_image()'
+    assert props
+    props.user_home = '/testHome/'
+    props.username = 'sanity-user'
+    props.share_path('/inception/sanity-home-dir',
+                     os.path.join(props.user_home, 'subdir'))
+END
+mkdir sanity-home-dir
+karton image import sanity-image definition-dir/
+echo
+
+echo "KARTON BUILD ..."
+karton build sanity-image
+echo
+
+echo "KARTON RUN ..."
+if karton run sanity-image true 2> /dev/null; then
+    echo "Run should have failed as \$PWD is not shared."
+    exit 1
+fi
+cd sanity-home-dir
+curr_dir=\$(karton run sanity-image pwd)
+if [[ "\$curr_dir" != *"/testHome/subdir"* ]]; then
+    echo "Current directory inside the sanity image is wrong: \$curr_dir"
+    exit 1
+fi
+uname=\$(karton run sanity-image uname)
+if [[ \$uname != "Linux"* ]]; then
+    echo "Wrong uname output: \$uname?"
+    exit 1
+fi
+issue=\$(karton run sanity-image cat /etc/issue)
+if [[ \$issue != *"Ubuntu"* ]]; then
+    echo "Wrong /etc/issue content: \$issue"
+    exit 1
+fi
+echo
 EOF
 
 for target in $TARGETS; do
     ./inception/inception.py \
         --add "$package_path" _ \
         --save-back "test-results/inception-$target.json"  "test-results.json" \
-        --add-script "$script_path" "run.sh" \
+        --save-back "test-results/sanity-$target.log"  "sanity-results.log" \
+        --add-script "$main_script_path" "run.sh" \
+        --add-script "$sanity_script_path" "sanity.sh" \
         "$target" \
         "./run.sh"
 done
--- tests/summary.py
+++ tests/summary.py
@@ -54,6 +54,41 @@ def process_results_file(summary, target, results_path):
             })
 
 
+def process_sanity_file(summary, target, sanity_path):
+    '''
+    Process a sanity log file and adds its data to `summary`.
+
+    summary:
+        The dictionary cotaining the summary of all test results.
+    target:
+        The name of the target.
+    sanity_path:
+        The path where the sanity results for `target` are.
+    '''
+    summary['sanity-count'] += 1
+
+    with open(sanity_path, 'r') as sanity_file:
+        lines = sanity_file.readlines()
+
+    result = lines[0].strip()
+
+    if result == 'PASSED':
+        summary['sanity-pass-count'] += 1
+
+    elif result == 'SKIPPED':
+        summary['sanity-skip-count'] += 1
+
+    elif result == 'FAILED':
+        summary['sanity-fail-count'] += 1
+        summary['sanity-failure-details'].append({
+            'target': target,
+            'log': ''.join(lines[5:])
+            })
+
+    else:
+        assert False, 'Invalid sanity result: %s' % result
+
+
 def main():
     '''
     Generates `test-results/summary.json` and prints out a summary as well.
@@ -65,6 +100,12 @@ def main():
         'error-count': 0,
         'total-count': 0,
         'failure-details': {},
+
+        'sanity-count': 0,
+        'sanity-pass-count': 0,
+        'sanity-skip-count': 0,
+        'sanity-fail-count': 0,
+        'sanity-failure-details': [],
         }
 
     for path in glob.glob('test-results/*.json'):
@@ -78,17 +119,27 @@ def main():
 
         process_results_file(summary, target, path)
 
+    for path in glob.glob('test-results/sanity-*.log'):
+        filename = os.path.basename(path)
+        target = filename[len('sanity-'):-len('.log')]
+        process_sanity_file(summary, target, path)
+
     with open('test-results/summary.json', 'w') as summary_file:
         json.dump(summary,
                   summary_file,
                   indent=4,
                   separators=(',', ': '))
 
-    summary['ok'] = not summary['fail-count'] and not summary['error-count']
+    summary['ok'] = \
+        not summary['fail-count'] and \
+        not summary['error-count'] and \
+        not summary['sanity-fail-count']
 
     def print_counts():
-        print('Ran %d tests against %d targets.' %
-              (summary['total-count'], summary['target-count']))
+        print('Ran %d tests and %d sanity checks against %d targets.' %
+              (summary['total-count'],
+               summary['sanity-count'],
+               summary['target-count']))
         print()
 
     print()
@@ -103,8 +154,25 @@ def main():
                 print('-' * 70)
             print()
 
+        for detail in summary['sanity-failure-details']:
+            print('=' * 70)
+            print('SANITY FAIL on target %s' % detail['target'])
+            print('-' * 70)
+            log_lines = detail['log'].split('\n')
+            max_lines = 12
+            if log_lines > max_lines:
+                log_lines = detail['log'].split('\n')[-max_lines:]
+                log_lines.insert(0, '[...]')
+            log = '\n'.join(log_lines)
+            print(log.strip())
+            print('-' * 70)
+            print()
+
         print_counts()
-        print('FAILED (%s failure, %d errors)' % (summary['fail-count'], summary['error-count']))
+        print('FAILED (%s failures, %d errors, %d sanity failures)' %
+              (summary['fail-count'],
+               summary['error-count'],
+               summary['sanity-fail-count']))
     else:
         print_counts()
         print('SUCCESS')
