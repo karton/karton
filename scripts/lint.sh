72cadc999eab61d0a4e2775da4f6bbc3d0ed4070
Marco Barisione <marco@barisione.org>
2019-08-26 18:09:55 +0100

    Add support for linting with pylint --py3k mode and pycodestyle

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -13,6 +13,28 @@ function error_exit() {
     exit 1
 }
 
+function join() {
+    local IFS="$1"
+    shift
+    echo "$*"
+}
+
+failure_count=0
+total_count=0
+
+function error_or_success() {
+    ret="$1"
+    echo
+    if [[ "$ret" = 0 ]]; then
+        echo "Success!"
+    else
+        echo "Failed!"
+        ((failure_count++))
+    fi
+    ((total_count++))
+    echo
+}
+
 cd "$top_dir" || error_exit "Cannot change directory to $top_dir"
 
 if [[ $# = 0 ]]; then
@@ -30,8 +52,62 @@ fi
 
 [[ ${#py_files[@]} != 0 ]] || error_exit "No Python files in the repository?"
 
+
+# Pylint
+
+echo "== PYLINT == "
+
 pylint \
     --rcfile=scripts/pylintrc \
     --reports=n \
     --score=n \
     "${py_files[@]}"
+error_or_success $?
+
+
+# Pylint (Python 3 mode)
+
+echo "== PYLINT (PYTHON 3 MODE) == "
+
+pylint \
+    --py3k \
+    --disable useless-suppression \
+    --rcfile=scripts/pylintrc \
+    --reports=n \
+    --score=n \
+    "${py_files[@]}"
+error_or_success $?
+
+
+# Pycodestyle
+
+echo "== PYCODESTYLE == "
+
+pycodestyle_ignore=(
+    'E123' # Closing bracket indentation. Checked by pylint.
+    'E124' # Closing bracket not aligned. Pylint has different opinions.
+    'E241' # Multiple spaces after colon. Allowed for dicts.
+    'E261' # Two spaces before inline comment.
+    'E266' # Too many "#". It's useful to define blocks of code.
+    'E402' # Module level import not at the top. Checked by pylint.
+    'E501' # Line too long. Checked by pylint.
+    'E701' # Multiple statements in one line. Checked by pylint.
+    'E722' # Bare except. Checked by pylint.
+    'E731' # Do not assign lambda. Needed when defining argument to avoid a function redef error.
+    'E741' # Ambiguous variable name. Pylint already checks for names.
+    'W504' # Line break after binary operator. This is the recommended style (503 is the opposite).
+    )
+pycodestyle \
+    --ignore="$(join "," "${pycodestyle_ignore[@]}")" \
+    "${py_files[@]}"
+error_or_success $?
+
+
+# Done!
+
+if [[ $failure_count = 0 ]]; then
+    echo "All tests passed"
+else
+    echo "Failures: $failure_count out of $total_count tests"
+fi
+exit $failure_count
